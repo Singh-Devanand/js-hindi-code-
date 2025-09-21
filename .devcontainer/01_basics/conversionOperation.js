6db7cce4700a356e970384b69a22a7b517eaa67b
Singh-Devanand <devanandsingh61157@gmail.com>
2025-09-21 14:01:13 +0000

    conversion of datatype

---
+++ .devcontainer/01_basics/conversionOperation.js
@@ -0,0 +1,25 @@
+let score="33"
+console.log(typeof score)
+
+let valueIntNumber=Number(score)
+console.log(typeof valueIntNumber)
+
+/*
+"33" =>33
+"33abc"=>NaN
+true=> 1; false=>0
+
+*/
+let isLoggedIn = 1
+let isBoolean=Boolean(isLoggedIn)
+console.log(isBoolean)
+/*
+1=>true
+0=>false
+""=>false
+"deva"=>true
+
+*/
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
