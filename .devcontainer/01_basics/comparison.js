ed7dac6c878d341843153d1cf98fcacafd37ed6a
Singh-Devanand <devanandsingh61157@gmail.com>
2025-09-22 04:07:17 +0000

    Add comparison and operation examples

---
+++ .devcontainer/01_basics/comparison.js
@@ -0,0 +1,21 @@
+console.log(2>1)
+console.log(2<1)
+console.log(2==1)
+console.log(2>=1)
+console.log(2!=1)
+
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(nul>=0)
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+
+// strict check => ye value ke sath sath datatype ko bhi check karta hai
+
+console.log("2"===2)
